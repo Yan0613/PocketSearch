32058c3a89fcd0bb9fbb8a289da2b190eef38b37
Yan0613 <1020303879@qq.com>
2023-12-22 21:27:33 +0800

    增加搜视频功能-接入Bilibili

---
+++ src/main/java/com/yupi/springbootinit/model/vo/VideoVO.java
@@ -0,0 +1,18 @@
+package com.yupi.springbootinit.model.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class VideoVO implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private String author;
+    private String url;
+    private String title;
+    private String pic;
+}
